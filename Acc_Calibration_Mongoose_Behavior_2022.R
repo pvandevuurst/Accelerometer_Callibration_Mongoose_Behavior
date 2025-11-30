efb8a40fe706cdcdf317076172428f46c729bf66
Paige Van de Vuurst <71039988+pvandevuurst@users.noreply.github.com>
2025-11-30 13:54:25 -0500

    Update Acc_Calibration_Mongoose_Behavior_2022.R
    Code for analyses in "Identifying social behaviours related to disease transmission in banded mongoose from accelerometer data
    Authored By Paige Van de Vuurst and Kathleen A. Alexander
    First published: 11 July 2023 in Ecological Solutions and Evidence
    Available at: https://doi.org/10.1002/2688-8319.12228

--- Acc_Calibration_Mongoose_Behavior_2022.R
+++ Acc_Calibration_Mongoose_Behavior_2022.R
@@ -1,3 +1,7 @@
+#Code for analyses in "Identifying social behaviours related to disease transmission in banded mongoose from accelerometer data" By Paige Van de Vuurst, Kathleen A. Alexander
+#First published: 11 July 2023 in Ecological Solutions and Evidence
+#Available at: https://doi.org/10.1002/2688-8319.12228
+
 #Begin by setting a working directory (i.e., file where all your data is stored)
 setwd("C:/Users/paige/Desktop/Data/Accele_Data_AlexLab")
 
@@ -147,3 +151,4 @@ alert_plot1<-ggplot(alert, aes(x=t)) + geom_line(aes(y = MagX_c), color = "blue"
   scale_x_discrete(guide = guide_axis(check.overlap=T))
 
 alert_plot1
+
